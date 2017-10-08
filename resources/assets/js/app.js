ef0a79c66018242de60462e8889f5f30f855b9e5
Cameron Smith <gitcontrol@outlook.com>
2017-10-08 19:44:50 +0100

    Added datepicker

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,12 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+const flatpickr = require("flatpickr");
+const $ = require("jquery");
 
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
+$(document).ready(function() {
+    flatpickr(".flatpickr", {
+        dateFormat: "d/m/Y",
+        altFormat: "J F Y",
+        altInput: true
+    });
 });
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,9 @@
 @extends ('layouts.master')
 
 @section ('content')
-    Homepage
+    <form method="POST" action="/">
+        {{ csrf_field() }}
+        <input name="date" class="flatpickr flatpickr-input" type="text" placeholder="Select Date.." value="">
+        <input type="submit" value="save">
+    </form>
 @endsection
