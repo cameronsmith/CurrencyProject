138a21f114916fa020a87edcf85d5a43bf15df26
Cameron Smith <gitcontrol@outlook.com>
2017-10-08 18:58:05 +0100

    Added bulma + basic layout page + controller

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Currency Project - by Cameron Smith</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+<header class="section">
+    <div class="container">
+        <h1 class="title">Currency Project by Cameron Smith</h1>
+        <h2 class="subtitle">
+            A simple currency project to get the rates based on years.
+        </h2>
+    </div>
+</header>
+@yield('content')
+</body>
+</html>
